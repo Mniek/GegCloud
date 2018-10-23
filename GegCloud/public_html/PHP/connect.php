83882ba9220a763c6d5d2439b4ff43b825dd822e
Mniek <mniek890@o2.pl>
2018-10-23 23:28:00 +0200

    Przygotowane pod logowanie

--- GegCloud/public_html/PHP/connect.php
+++ GegCloud/public_html/PHP/connect.php
@@ -1,1 +1,15 @@
 
+<?php
+    $host = 'localhost';
+    $user = 'olmbm';
+    $pass = 'Gegesklad234';
+    $db = 'olmbm';
+    
+    $conn = mysqli_connect($host,$user,$pass,$db);
+        if($conn)
+        {
+            echo 'Helo';
+        }
+            
+
+?>
